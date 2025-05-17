1fa1d6d0be2c20e4677ca8a5269a2a4b32390be2
Stephan Kulla <git.mail@kulla.me>
2025-05-17 18:29:49 +0200

    feat: Add Quill

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+import Quill from 'quill'
+import QuillCursors from 'quill-cursors'
+import { useEffect, useRef } from 'react'
 import './App.css'
+import 'quill/dist/quill.snow.css'
+
+Quill.register('modules/cursors', QuillCursors)
 
 export default function App() {
+  const quill = useRef<Quill>(null)
+  const editorDiv = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (editorDiv.current && !quill.current) {
+      quill.current = new Quill(editorDiv.current, {
+        modules: {
+          cursors: true,
+          toolbar: [
+            [{ header: [1, 2, false] }],
+            ['bold', 'italic', 'underline'],
+            ['image', 'code-block'],
+          ],
+          history: {
+            // Local undo shouldn't undo changes
+            // from remote users
+            userOnly: true,
+          },
+        },
+        placeholder: 'Start collaborating...',
+        theme: 'snow',
+      })
+    }
+  })
+
   return (
     <main className="prose p-10">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Quill text editor</h1>
+      <div id="editor" ref={editorDiv} className="w-full">
+        Hello World
+      </div>
     </main>
   )
 }
